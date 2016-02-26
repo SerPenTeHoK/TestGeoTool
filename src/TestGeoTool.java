c03963e806c8c76e638ecfa14fe9ddb07a6f1027
GSV <sergey-for-fotos@mail.ru>
2016-02-26 11:38:29 +0300

    Home wor look WFS

--- src/TestGeoTool.java
+++ src/TestGeoTool.java
@@ -1,17 +1,24 @@
 
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 import org.geoserver.gwc.GWC;
 import org.geoserver.gwc.wms.CachingWebMapService;
-import org.geotools.data.CachingFeatureSource;
-import org.geotools.data.FeatureSource;
+import org.geoserver.wfs.WebFeatureService;
+
+import org.geotools.data.*;
 import org.geotools.data.ows.Layer;
 import org.geotools.data.wms.WMS1_0_0;
 import org.geotools.data.wms.WebMapServer;
 import org.geotools.data.wms.request.GetMapRequest;
 import org.geotools.data.wms.response.GetMapResponse;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.factory.GeoTools;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContent;
 import org.geotools.map.WMSLayer;
@@ -21,6 +28,13 @@ import org.geotools.swing.wms.WMSLayerChooser;
 import org.geotools.util.ObjectCache;
 import org.geotools.util.ObjectCaches;
 import org.geowebcache.GeoWebCache;
+import org.opengis.feature.Feature;
+
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.spatial.Intersects;
+
 import org.xml.sax.SAXException;
 import sun.swing.ImageCache;
 
@@ -31,8 +45,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 
 import com.vividsolutions.jts.geom.Polygonal;
 import com.vividsolutions.jts.geom.Polygon;
@@ -49,9 +63,54 @@ public class TestGeoTool extends JFrame  {
         }
         */
         //testWebServer();
+/*
+        String getCapabilities = "http://localhost:8080/geoserver/wfs?REQUEST=GetCapabilities";
+
+        Map connectionParameters = new HashMap();
+        connectionParameters.put("WFSDataStoreFactory:GET_CAPABILITIES_URL", getCapabilities );
+
+// Step 2 - connection
+        DataStore data = DataStoreFinder.getDataStore( connectionParameters );
+
+// Step 3 - discouvery
+        String typeNames[] = data.getTypeNames();
+        String typeName = typeNames[0];
+        SimpleFeatureType schema = data.getSchema( typeName );
+
+// Step 4 - target
+        FeatureSource<SimpleFeatureType, SimpleFeature> source = data.getFeatureSource( typeName );
+        System.out.println( "Metadata Bounds:"+ source.getBounds() );
+
+// Step 5 - query
+        String geomName = schema.getDefaultGeometry().getLocalName();
+        Envelope bbox = new Envelope( -100.0, -70, 25, 40 );
+
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2( GeoTools.getDefaultHints() );
+        Object polygon = JTS.toGeometry( bbox );
+        Intersects filter = ff.intersects( ff.property( geomName ), ff.literal( polygon ) );
+
+        Query query = new DefaultQuery( typeName, filter, new String[]{ geomName } );
+        FeatureCollection<SimpleFeatureType, SimpleFeature> features = source.getFeatures( query );
+
+        ReferencedEnvelope bounds = new ReferencedEnvelope();
+        Iterator<SimpleFeature> iterator = features.iterator();
+        try {
+            while( iterator.hasNext() ){
+                Feature feature = (Feature) iterator.next();
+                bounds.include( feature.getBounds() );
+            }
+            System.out.println( "Calculated Bounds:"+ bounds );
+        }
+        finally {
+            features.close( iterator );
+        }
+*/
 
 
-        URL capabilitiesURL = new URL("http://192.168.1.80:8180/geoserver/wms?service=WMS&request=GetCapabilities");
+        // Home
+        //URL capabilitiesURL = new URL("http://192.168.1.80:8180/geoserver/wms?service=WMS&request=GetCapabilities");
+        // Work
+        URL capabilitiesURL = new URL("http://localhost:8080/geoserver/wms?service=WMS&request=GetCapabilities");
         WebMapServer wms = new WebMapServer( capabilitiesURL );
 
         List<Layer> wmsLayers = WMSLayerChooser.showSelectLayer( wms );
@@ -62,36 +121,15 @@ public class TestGeoTool extends JFrame  {
         MapContent mapcontent = new MapContent();
         mapcontent.setTitle( wms.getCapabilities().getService().getTitle() );
 
-        //ImageCache imageCache = new ImageCache();
-        //ObjectCache objectCache = new ObjectCache()
-        //ObjectCaches objectCaches = new ObjectCaches();
-
-
-        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory( null );
-
-        /*
-        WKTReader reader = new WKTReader( geometryFactory );
-        Polygon polygon = (Polygon) reader.read("POLYGON((20 10, 30 0, 40 10, 30 20, 20 10))");
-        */
-        FeatureSource featureSource = new FeatureSource()
-        }
-        FeatureLayer featureLayer = new FeatureLayer();
-
-        //mapcontent.addLayer(Polygon);
-
         for( Layer wmsLayer : wmsLayers ){
             WMSLayer displayLayer = new WMSLayer(wms, wmsLayer );
             mapcontent.addLayer(displayLayer);
         }
-        //GeoWebCache geoWebCache = new GeoWebCache();
-        //GWC gwc = new GWC();
-        //CachingWebMapService cachingWebMapService = new CachingWebMapService();
-
-        //FeatureSource featureSource = new FeatureSource();
-        //CachingFeatureSource cachingFeatureSource = new CachingFeatureSource();
 
         // Now display the map
         JMapFrame.showMap(mapcontent);
+        
+
     }
     private static void testShpFile() throws IOException {
         /*
@@ -112,6 +150,7 @@ public class TestGeoTool extends JFrame  {
         Layer layer = new FeatureLayer(cache, style);
         map.addLayer(layer);
         // Now display the map
+
         JMapFrame.showMap(map);
 */
     }
